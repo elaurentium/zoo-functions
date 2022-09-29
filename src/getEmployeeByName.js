55e5787065637a600c41f30a1231d7ddc9292bb4
Evandro <109320830+elaurentium@users.noreply.github.com>
2022-09-29 15:41:18 -0300

    getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) return {};
+  return employees.find((name) =>
+    name.firstName === employeeName || name.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
