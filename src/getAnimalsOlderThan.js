addee35251c93381ab53530d854f6ea117dfd99f
Evandro <109320830+elaurentium@users.noreply.github.com>
2022-09-29 15:19:18 -0300

    getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.find((a) => a.name === animal).residents.every((anim) => anim.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
