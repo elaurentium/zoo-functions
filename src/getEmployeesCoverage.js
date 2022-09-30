13560c2a5fb7c49cc3264cfa033bf17e02cc99ca
Evandro <109320830+elaurentium@users.noreply.github.com>
2022-09-30 16:30:37 -0300

    getSchedule

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const { species, employees } = data;
+
+const arrayResult = employees.map((e) => {
+  const arr = {
+    id: e.id,
+    fullName: `${e.firstName} ${e.lastName}`,
+    species: e.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).name),
+    location: e.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).location),
+  };
+  return arr;
+});
+
+function getEmployeesCoverage(param) {
   // seu código aqui
+  if (param === undefined) return arrayResult;
+
+  const validEntry = arrayResult.find((element) => element.fullName.includes(Object.values(param))
+  || element.id.includes(Object.values(param)));
+
+  if (validEntry === undefined) {
+    throw new Error('Informações Inválidas');
+  }
+  return validEntry;
 }
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { species, hours } = data;
+
+function returnObject() {
+  const object = {};
+
+  Object.keys(hours).forEach((day) => {
+    if (day === 'Monday') {
+      object[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      object[day] = {
+        officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+        exhibition: species.filter((specie) =>
+          specie.availability.includes(day)).map((elemento) => elemento.name),
+      };
+    }
+  });
+
+  return object;
 }
+
+function checkParam(param) {
+  if (species.filter((element) => element.name).map((element) =>
+    element.name).some((element) => element === param)) {
+    return species.find((specie) => specie.name === param).availability;
+  }
+  if (Object.keys(hours).includes(param)) {
+    return { [param]: returnObject()[param] };
+  }
+  return returnObject();
+}
+
+function getSchedule(scheduleTarget) {
+  if (scheduleTarget === undefined) {
+    return returnObject();
+  }
+  return checkParam(scheduleTarget);
+}
+
+console.log(getSchedule('elephants'));
 
 module.exports = getSchedule;
