2303e52e882d71f34bfa246ecdb0a07e6d343942
Evandro <109320830+elaurentium@users.noreply.github.com>
2022-09-30 16:46:10 -0300

    getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const { employees } = data;
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = employees.find((e) => e.id === id).responsibleFor[0];
+  const specie = species.find((e) => e.id === employee).residents;
+  return Object.values(specie.sort((age1, age2) => age1.age - age2.age)[specie.length - 1]);
 }
 
 module.exports = getOldestFromFirstSpecies;
